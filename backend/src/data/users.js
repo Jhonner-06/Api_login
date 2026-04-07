a46b83ab8b806b2336fd3f2e06cf77055c7a453a
Jhonner-06 <jhonnermoreno.123@hotmail.com>
2026-04-06 23:36:42 -0500

    Se añade configuracion del backend con un array que almacena datos para no conectar una base de datos

---
+++ backend/src/data/users.js
@@ -0,0 +1,18 @@
+const Usuarios = [
+    {
+        id: 1,
+        nombre: "Jhonner",
+        apellido: "Moreno",
+        contraseña: "12345"
+    },
+
+    {
+        id: 2,
+        nombre: "Camilo",
+        apellido: "Perez",
+        contraseña: "654321"
+    }
+
+] 
+
+module.exports = Usuarios;
