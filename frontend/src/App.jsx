e50c86e5a8341f34af4592aa73bd349f238d4dbd
Jhonner-06 <jhonnermoreno.123@hotmail.com>
2026-04-07 01:09:47 -0500

    Se añade configuracion del fronten la cual nos lleva a otra pagina al iniciar secion y se añaden un poco de estilos

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,33 @@
 import { useState } from 'react'
 import axios from 'axios'
+import { Routes, Route, useNavigate } from 'react-router-dom' 
+import Inicio from './Inicio'
+import './App.css'
 
 
-function App() {
+function Login({ setMensaje, mensaje }) {
 
   const [nombre, setNombre ] = useState('')
   const [contraseña, setContraseña ] = useState('')
-  const [mensaje, setMensaje ] = useState('')
+  const conexion = "http://localhost:5000/api/login"
+  const navigate = useNavigate()
 
   const handleLogin = async (e) =>{
     e.preventDefault()
 
     try{
-      const respuesta = await axios.post('http://localhost:5000/api/login'
+      const respuesta = await axios.post(conexion
         ,{
           nombre,
           contraseña
         }
       )
       
-    setMensaje(respuesta.data.message);
-    console.log("token recibido", respuesta.data.token)
+      localStorage.setItem('token', respuesta.data.token);
+      localStorage.setItem('usuario', JSON.stringify(respuesta.data.usuario));
+
+      
+      navigate('/inicio');
 
     }catch (error) {
       setMensaje(error.response?.data?.menssage || "Error al conectar")
@@ -30,26 +37,26 @@ function App() {
 
 
   return (
-      <div style={{ padding: '20px', textAlign: 'center' }}>
+      <div  className="segdiv" style={{ padding: '20px', textAlign: 'center' }}>
         <h2>Mi Sistema de Login</h2>
         <form onSubmit={handleLogin}>
-          <input 
-            type="text" 
-            placeholder="Usuario" 
-            onChange={(e) => setNombre(e.target.value)} 
-          />
-          <br /><br />
-          <input 
-            type="password" 
-            placeholder="Contraseña" 
-            onChange={(e) => setContraseña(e.target.value)} 
-          />
-          <br /><br />
+          <input type="text" placeholder="Usuario" onChange={(e) => setNombre(e.target.value)} /><br /><br />
+          <input type="password" placeholder="Contraseña" onChange={(e) => setContraseña(e.target.value)} /><br /><br />
           <button type="submit">Entrar</button>
         </form>
-
         {mensaje && <p><strong>{mensaje}</strong></p>}
       </div>
+    )
+  }
+
+  function App() {
+    const [mensaje, setMensaje] = useState('')
+
+    return (
+      <Routes>
+        <Route path="/" element={<Login setMensaje={setMensaje} mensaje={mensaje} />} />
+        <Route path="/inicio" element={<Inicio />} />
+      </Routes>
     )
 }
 
